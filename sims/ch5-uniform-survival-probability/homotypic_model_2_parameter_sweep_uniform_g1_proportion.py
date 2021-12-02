bdc7e6657d8bbd0bf63076e47aad69954f004d9e
Thomas Pak <thomas.pak@linacre.ox.ac.uk>
2021-12-02 19:22:23 +0000

    Import simulations libs

--- sims/ch5-uniform-survival-probability/homotypic_model_2_parameter_sweep_uniform_g1_proportion.py
+++ sims/ch5-uniform-survival-probability/homotypic_model_2_parameter_sweep_uniform_g1_proportion.py
@@ -1,6 +1,8 @@
 import numpy as np
 import sys
 
+import sys
+sys.path.append('../lib')
 from heterotypic_model_2_parameter_sweep import heterotypic_model_2_parameter_sweep_general
 
 # Homotypic uniform cell cycle model
