6cd22126d7d7f7b9d3267eeb4988c0a26b4aabd0
Thomas Pak <thomas.pak@linacre.ox.ac.uk>
2021-12-03 21:01:29 +0000

    Rename ch4 sim scripts

--- sims/ch4-proliferation-regimes/run-vertex-proliferation-regime-1.sh
+++ sims/ch4-proliferation-regimes/run-vertex-proliferation-regime-1.sh
@@ -2,9 +2,9 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=12:00:00
-#SBATCH --job-name=proliferation-regime-1-tests
-#SBATCH --output=proliferation-regime-1-tests_%A_%a.out
-#SBATCH --error=proliferation-regime-1-tests_%A_%a.err
+#SBATCH --job-name=proliferation-regime-1
+#SBATCH --output=proliferation-regime-1_%A_%a.out
+#SBATCH --error=proliferation-regime-1_%A_%a.err
 #SBATCH --array=0-899
 
 #SBATCH --mail-type=all
@@ -16,5 +16,5 @@ trap 'date "+%Y-%d-%m %T" | tr -d "\n" ; echo "::Exiting $0"' EXIT
 
 date "+%Y-%d-%m %T" | tr -d "\n" ; echo "::Entering $0"
 
-python3 generate-parameters-proliferation-regime-1-tests.py $SLURM_ARRAY_TASK_ID | \
+python3 generate-vertex-parameters-proliferation-regime-1.py $SLURM_ARRAY_TASK_ID | \
             "$CHASTE_BUILD/projects/cell-competition/apps/DeathClockApp"
