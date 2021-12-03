228788c1f1ea45f4f291e4ca159e2f660d2bc02c
Thomas Pak <thomas.pak@linacre.ox.ac.uk>
2021-12-03 21:04:35 +0000

    Rename ch5 exp survival probability sim scripts

--- sims/ch5-exponential-survival-probability/run-vertex-exponential-survival-probability.sh
+++ sims/ch5-exponential-survival-probability/run-vertex-exponential-survival-probability.sh
@@ -2,9 +2,9 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=48:00:00
-#SBATCH --job-name=g1-proportion-tests
-#SBATCH --output=g1-proportion-tests_%A_%a.out
-#SBATCH --error=g1-proportion-tests_%A_%a.err
+#SBATCH --job-name=exponential-survival-probability
+#SBATCH --output=exponential-survival-probability_%A_%a.out
+#SBATCH --error=exponential-survival-probability_%A_%a.err
 #SBATCH --array=0-299
 
 #SBATCH --mail-type=all
@@ -16,5 +16,5 @@ trap 'date "+%Y-%d-%m %T" | tr -d "\n" ; echo "::Exiting $0"' EXIT
 
 date "+%Y-%d-%m %T" | tr -d "\n" ; echo "::Entering $0"
 
-python3 generate-parameters-g1-proportion-tests.py $SLURM_ARRAY_TASK_ID | \
+python3 generate-vertex-parameters-exponential-survival-probability.py $SLURM_ARRAY_TASK_ID | \
             "$CHASTE_BUILD/projects/cell-competition/apps/DeathClockApp"
