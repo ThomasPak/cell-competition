034bf62d7cc0c210d27d2e5e581eb5170ef576f1
Thomas Pak <thomas.pak@linacre.ox.ac.uk>
2021-12-03 21:05:05 +0000

    Rename output directory

--- sims/ch4-proliferation-regimes/generate-vertex-parameters-proliferation-regime-3.py
+++ sims/ch4-proliferation-regimes/generate-vertex-parameters-proliferation-regime-3.py
@@ -8,7 +8,7 @@ if len(argv) != 2:
 
 # This part of the input is the same for all simulations in this suite
 base_string = """
-output-directory = proliferation-regime-3-tests
+output-directory = proliferation-regime-3
 
 simulation-time = 100000
 dt = 0.005
